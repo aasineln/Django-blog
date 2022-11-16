1cd15da36b7a600f8dde99b00f486d5dd18f557b
Alexey Sinelnikov <alex.sinelnikof@gmail.com>
2022-11-16 19:15:47 +0300

    06-hw-admin done

--- 05_Forms/news/app_news/admin.py
+++ 05_Forms/news/app_news/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Comment
+
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
 
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'created_at', 'updated_at', 'is_active']
+    list_filter = ['is_active']
+    inlines = [CommentInLine]
+    actions = ['mark_active', 'mark_inactive']
+
+    def mark_active(self, request, queryset):
+        queryset.update(is_active=True)
+
+    def mark_inactive(self, request, queryset):
+        queryset.update(is_active=False)
+
+    mark_active.short_description = 'Перевести в статус active'
+    mark_inactive.short_description = 'Перевести в статус inactive'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['news', 'name', 'short_comment', 'created', 'updated']
+    list_filter = ['name']
+    actions = ['deleted_by_admin']
+
+    def deleted_by_admin(self, request, queryset):
+        queryset.update(text='Удалено администратором')
+
+    deleted_by_admin.short_description = 'Очистить текст и подписать Удалено админстратором'
