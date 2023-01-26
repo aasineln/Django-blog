4c14ac236ccf51ff58bcf8baecb112aa48607a8f
Alexey Sinelnikov <30474984+aasineln@users.noreply.github.com>
2023-01-26 03:13:29 +0300

    Delete 04_DatabasesAndModels/board directory

--- 04_DatabasesAndModels/board/advertisements_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from advertisements_app.models import Advertisement
-
-
-@admin.register(Advertisement)
-class AdvertisementAdmin(admin.ModelAdmin):
-    pass
--- 04_DatabasesAndModels/board/advertisements_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .views import AdvertisementListView, AdvertisementDetailView
-
-urlpatterns = [
-    path('advertisements/', AdvertisementListView.as_view(), name='advertisement'),
-    path('advertisements/<int:pk>/', AdvertisementDetailView.as_view(), name='advertisement-detail')
-]
